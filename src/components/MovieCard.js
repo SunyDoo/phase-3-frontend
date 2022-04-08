4c310f6881f280d543cc99710779eee065f60028
Sunil <snilavarath@gmail.com>
2022-04-08 16:04:26 -0400

    average score

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,20 +3,36 @@ import React, { useState, useEffect } from "react";
 function MovieCard({ movie }) {
   const [scores, setScores] = useState([]);
   const [display, setDisplay] = useState(false);
+  const [average, setAverage] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:9292/movies/${movie.id}`)
       .then((r) => r.json())
       .then((movie) => setScores(movie.reviews));
   }, []);
-  
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/movies/${movie.id}/average_score`)
+      .then((r) => r.json())
+      .then((movie) => setAverage(movie.reviews));
+  }, []);
+
   function handleClick() {
     setDisplay((display) => !display);
   }
 
+  const findAverageAge = (arr) => {
+    return arr.reduce((acc, val) => {
+      return acc + val.score / arr.length;
+    }, 0);
+  };
+
   return (
     <li>
-      <h1>{movie.title}</h1>
+      <h1>
+        {movie.title} 
+      </h1>
+      <p>Average Score: {findAverageAge(average)}</p>
       <img src={movie.image} alt={movie.title} onClick={handleClick} />
       <span>
         <ol style={display ? {} : { display: "none" }}>
